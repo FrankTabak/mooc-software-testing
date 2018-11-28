934cba5edc3eed53783740a30f401e8eb8cae2a8
ORTECADS\FrankT <frank.tabak@ortec.com>
2018-11-28 16:32:40 +0100

    Make roman attribute of Test

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -5,31 +5,28 @@ import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private final RomanNumeral roman = new RomanNumeral();
 
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
